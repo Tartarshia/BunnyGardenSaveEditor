f5626da45d2a9e058088d60bde9fc6ea71c897f9
Tatasha <tartarshia@gmail.com>
2026-09-24 22:05:55 -0400

    Fix native release packaging

--- package.cmd
+++ package.cmd
@@ -5,6 +5,8 @@ if errorlevel 1 exit /b 1
 if not exist "%~dp0dist" mkdir "%~dp0dist"
 set "ZIP=%~dp0dist\BunnyGardenSaveEditor-v1.0.0-windows.zip"
 if exist "%ZIP%" del /q "%ZIP%"
-tar.exe -a -c -f "%ZIP%" -C "%~dp0" BunnyGardenSaveEditor.exe Launch-BunnyGardenSaveEditor.cmd README.md ACHIEVEMENTS.md LICENSE
+pushd "%~dp0"
+tar.exe -a -c -f "%ZIP%" BunnyGardenSaveEditor.exe Launch-BunnyGardenSaveEditor.cmd README.md ACHIEVEMENTS.md LICENSE
+popd
 if errorlevel 1 exit /b 1
 certutil -hashfile "%ZIP%" SHA256
