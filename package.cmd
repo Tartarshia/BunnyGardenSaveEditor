97f9a950ac341d32575656adaa9e18782b0040b3
Tatasha <tartarshia@gmail.com>
2026-09-24 23:32:58 -0400

    Fix release archive file list

--- package.cmd
+++ package.cmd
@@ -6,7 +6,7 @@ if not exist "%~dp0dist" mkdir "%~dp0dist"
 set "ZIP=%~dp0dist\BunnyGardenSaveEditor-v1.0.0-windows.zip"
 if exist "%ZIP%" del /q "%ZIP%"
 pushd "%~dp0"
-tar.exe -a -c -f "%ZIP%" BunnyGardenSaveEditor.exe Launch-BunnyGardenSaveEditor.cmd README.md ACHIEVEMENTS.md LICENSE
+tar.exe -a -c -f "%ZIP%" BunnyGardenSaveEditor.exe Launch-BunnyGardenSaveEditor.cmd README.md LICENSE
 popd
 if errorlevel 1 exit /b 1
 certutil -hashfile "%ZIP%" SHA256
